2e9f0e2c14edc04efa3437d44770da06f7a5aba1
matt <143686761+ulixert@users.noreply.github.com>
2026-03-02 00:42:51 -0800

    chore: update node version

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
-
-import { defineConfig, env } from '@prisma/config';
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
